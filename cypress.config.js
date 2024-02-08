45c020ebc386a110f506672d1aadef89e3fe67d4
Shariar Khan <shuvho.91@gmail.com>
2024-02-08 12:34:51 +0600

    cypresscloud project id added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "niztax",
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
