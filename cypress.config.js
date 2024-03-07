4a89af66b1dd2470bf57aca82d6b4a31f1104103
Shariar Khan <shuvho.91@gmail.com>
2024-03-07 20:57:23 +0600

    multiple env management system added

--- cypress.config.js
+++ cypress.config.js
@@ -1,39 +1,40 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-  projectId: "niztax",
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: "cypress/reports",
-    charts: true,
-    reportPageTitle: 'Automation Test Report',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-    code: false,
-  },
-  e2e: {
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on)
+    projectId: "niztax",
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+        reportDir: "cypress/reports",
+        charts: true,
+        reportPageTitle: 'Automation Test Report',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: false,
+        code: false,
     },
-    // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
-    'env': {
-      baseUrl:"https://automationexercise.com",
+    e2e: {
+        setupNodeEvents(on, config) {
+            require('cypress-mochawesome-reporter/plugin')(on)
+            const environment = config.env.environment || 'prod'
+            config.env = require(`./cypress/config/${environment}.json`)
+            config.baseUrl = config.env.baseUrl;
+            return config;
+        },
+        // Configure your E2E tests here
+        specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+        //to run the entire folder in interactive mode
+        experimentalRunAllSpecs: true,
+        testIsolation: false
     },
-    //to run the entire folder in interactive mode
-    experimentalRunAllSpecs: true,
-    testIsolation: false
-  },
-  pageLoadTimeout: 80000,
-  watchForFileChanges: false,
-  video: true,
-  screenshots: false,
-  headless: true,
-  retries: {
-    runMode: 2,
-    openMode: 0,
-  },
-  viewportWidth: 1320,
-  viewportHeight: 1080,
+    pageLoadTimeout: 80000,
+    watchForFileChanges: false,
+    video: true,
+    screenshots: false,
+    headless: true,
+    retries: {
+        runMode: 2,
+        openMode: 0,
+    },
+    viewportWidth: 1320,
+    viewportHeight: 1080,
 })
---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,5 @@
+describe('test', () => {
+    it('should test', () => {
+        cy.log(Cypress.env('baseUrl'))
+    });
+});
